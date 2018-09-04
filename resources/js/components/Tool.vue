a4bfc7e9575dbc1e02c3f17e7024b19babfd4037
Nathan Heffley <nathanheffley@gmail.com>
2018-09-03 22:49:42 -0400

    Add Previous/Next pagination links

--- resources/js/components/Tool.vue
+++ resources/js/components/Tool.vue
@@ -18,6 +18,30 @@
         <div v-else class="text-center">
             <svg class="spin-the-thing h-16 w-16 text-grey-darkest" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 3v2a5 5 0 0 0-3.54 8.54l-1.41 1.41A7 7 0 0 1 10 3zm4.95 2.05A7 7 0 0 1 10 17v-2a5 5 0 0 0 3.54-8.54l1.41-1.41zM10 20l-4-4 4-4v8zm0-12V0l4 4-4 4z"/></svg>
         </div>
+
+        <nav class="flex">
+            <button rel="prev"
+                    class="btn btn-link py-3 px-4"
+                    :class="{
+                        'text-primary dim': hasPreviousPage,
+                        'text-80 opacity-50': !hasPreviousPage,
+                    }"
+                    :disabled="!hasPreviousPage"
+                    @click.prevent="loadPreviousPage()">
+                Previous
+            </button>
+
+            <button rel="next"
+                    class="ml-auto btn btn-link py-3 px-4"
+                    :class="{
+                        'text-primary dim': hasNextPage,
+                        'text-80 opacity-50': !hasNextPage,
+                    }"
+                    :disabled="!hasNextPage"
+                    @click.prevent="loadNextPage()">
+                Next
+            </button>
+        </nav>
     </div>
 </template>
 
@@ -27,20 +51,42 @@
             return {
                 failedJobs: [],
                 showVendorTrace: false,
-                loading: true
+                loading: true,
+                page: 1,
             }
+        },
+        computed: {
+            path() {
+                return '/nova-vendor/kregel/nova-failed-jobs/failed-jobs?page=' + this.page;
+            },
+            hasNextPage() {
+                return !!this.failedJobs.next_page_url;
+            },
+            hasPreviousPage() {
+                return !!this.failedJobs.prev_page_url;
+            },
         },
         methods: {
             getFailedJobs() {
+                this.failedJobs = [];
                 this.loading = true;
-                axios.get('/nova-vendor/kregel/nova-failed-jobs/failed-jobs')
+                axios.get(this.path)
                     .then(res => {
                         this.failedJobs = res.data;
                         this.highlight()
                         this.loading = false;
                     });
             },
+            loadNextPage() {
+                this.page++;
+                this.getFailedJobs();
+            },
+            loadPreviousPage() {
+                this.page--;
+                this.getFailedJobs();
+            },
             refreshJobList() {
+                this.page = 1;
                 this.getFailedJobs();
             },
             toggleVendorTrace() {
